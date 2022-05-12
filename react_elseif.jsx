1c084b4e658f712c4bbd96bc695b6a3fea4a9eda
Aymeric Ratinaud <aratinau@users.noreply.github.com>
2022-05-12 22:34:14 +0200

    Update react_elseif.jsx

--- react_elseif.jsx
+++ react_elseif.jsx
@@ -6,8 +6,12 @@
     <ElseComponent/>
 )}
 
+/* no parentheses */
 {courierFile.name  ?
     <small className="text-primary">{courierFile.name}</small>
  :
     <small className="text-dark mb-0">{typeCourierFile(courierFile.type).label}</small>
 }
+
+/* one ligne */
+{courierFile.name ? courierFile.name : courierFile.filePath}
