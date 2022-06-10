d8a78696ead46ac6f3d76eea47dc3870427e2eb4
Aymeric Ratinaud <aratinau@users.noreply.github.com>
2022-06-10 16:36:21 +0200

    Create php_array.php

---
+++ php_array.php
@@ -0,0 +1,12 @@
+<?php
+
+// return index by value in a multi dimensionnal array
+
+function searchForId($id, $array) {
+   foreach ($array as $key => $val) {
+       if ($val['uid'] === $id) {
+           return $key;
+       }
+   }
+   return null;
+}
