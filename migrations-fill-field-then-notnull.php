9177240919628aebea417be5209d81dae292f7ff
Aymeric Ratinaud <aratinau@users.noreply.github.com>
2023-09-14 21:33:26 +0200

    Update migrations-fill-field-then-notnull.php

--- migrations-fill-field-then-notnull.php
+++ migrations-fill-field-then-notnull.php
@@ -10,3 +10,28 @@ public function up(Schema $schema): void
     $this->addSql('ALTER TABLE courier ALTER COLUMN courier_direction SET NOT NULL');
 }
 
+public function up(Schema $schema): void
+{
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE courier_file_type ADD auto_generate_courier_file BOOLEAN');
+    $this->addSql('UPDATE courier_file_type SET auto_generate_courier_file = false');
+    $this->addSql('ALTER TABLE courier_file_type ALTER COLUMN auto_generate_courier_file SET NOT NULL');
+}
+
+public function up(Schema $schema): void
+{
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE courier ADD unique_reference_id INT NULL');
+    $this->addSql('UPDATE courier SET unique_reference_id = id');
+    $this->addSql('ALTER TABLE courier ALTER COLUMN unique_reference_id SET NOT NULL');
+}
+
+public function up(Schema $schema): void
+{
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE courier ADD name_to VARCHAR(255) DEFAULT NULL');
+    $this->addSql('ALTER TABLE courier ALTER name_from DROP NOT NULL');
+    $this->addSql("UPDATE courier SET name_to = name_from WHERE direction = 'outcoming'");
+    $this->addSql("UPDATE courier SET name_from = NULL WHERE direction = 'outcoming'");
+    $this->addSql('ALTER TABLE courier ALTER direction TYPE VARCHAR(100)');
+}
